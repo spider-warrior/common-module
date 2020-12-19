b79cc644237721ed13f29d7c43b88ef28ffbcc11
yangjian <362961910@qq.com>
2020-12-19 14:51:50 +0800

    优化AppLogLayout

--- trace-common-module/logback-trace/src/main/java/cn/t/common/trace/logback/AppLogLayout.java
+++ trace-common-module/logback-trace/src/main/java/cn/t/common/trace/logback/AppLogLayout.java
@@ -2,9 +2,11 @@ package cn.t.common.trace.logback;
 
 import ch.qos.logback.classic.pattern.*;
 import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.IThrowableProxy;
 import ch.qos.logback.core.CoreConstants;
 import ch.qos.logback.core.LayoutBase;
 import cn.t.common.trace.generic.TraceConstants;
+import cn.t.util.common.ArrayUtil;
 import cn.t.util.common.DateUtil;
 import cn.t.util.common.JsonUtil;
 import cn.t.util.common.SystemUtil;
@@ -44,6 +46,7 @@ public class AppLogLayout extends LayoutBase<ILoggingEvent> {
     private static final String method = "method";
     private static final String line = "line";
     private static final String msg = "msg";
+    private static final String stackTrace = "stackTrace";
 
     @Override
     public String doLayout(ILoggingEvent event) {
@@ -59,10 +62,26 @@ public class AppLogLayout extends LayoutBase<ILoggingEvent> {
         map.put(method, methodOfCallerConverter.convert(event));
         map.put(line, lineOfCallerConverter.convert(event));
         map.put(msg, event.getFormattedMessage());
+        map.put(stackTrace, buildStackTrace(event.getThrowableProxy()));
         try {
             return JsonUtil.serialize(map) + CoreConstants.LINE_SEPARATOR;
         } catch (JsonProcessingException e) {
             return String.format(ERROR_PATTERN, map.toString()) + CoreConstants.LINE_SEPARATOR;
+        }
+    }
+
+    private String buildStackTrace(IThrowableProxy throwableProxy) {
+        if(throwableProxy == null) {
+            return null;
+        } else {
+            StringBuilder builder = new StringBuilder();
+            builder.append(throwableProxy.getClassName()).append(": ").append(throwableProxy.getMessage()).append(CoreConstants.LINE_SEPARATOR);
+            if(!ArrayUtil.isEmpty(throwableProxy.getStackTraceElementProxyArray())) {
+                for(int i=0; i<throwableProxy.getStackTraceElementProxyArray().length; i++) {
+                    builder.append(throwableProxy.getStackTraceElementProxyArray()[i]).append(CoreConstants.LINE_SEPARATOR);
+                }
+            }
+            return builder.toString();
         }
     }
 }
