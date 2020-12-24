a64d0ab7425cb8030749cfd0e652d9d85757fd55
yangjian <362961910@qq.com>
2020-12-24 16:35:29 +0800

    完善TraceIdGenerator

--- trace-common-module/trace-generic/src/main/java/cn/t/common/trace/generic/TraceIdGenerator.java
+++ trace-common-module/trace-generic/src/main/java/cn/t/common/trace/generic/TraceIdGenerator.java
@@ -15,6 +15,7 @@ public class TraceIdGenerator {
     private static final String PID = SystemUtil.getPid();
     //[service-name]-$pid-$mills-$tid
     private static final String TRACE_ID_PATTERN = "[%s]-%s-%d-%d";
+    private static final String REMOTE_TRACE_ID_PATTERN = "[%s][%s]-%s-%d-%d";
 
     public static String generateTraceId(String serviceName) {
         java.time.Instant.now();
@@ -22,5 +23,13 @@ public class TraceIdGenerator {
             serviceName = IP;
         }
         return String.format(TRACE_ID_PATTERN, serviceName, PID, SystemUtil.now(), Thread.currentThread().getId());
+    }
+
+    public static String generateTraceId(String serviceName, String remoteAddr) {
+        java.time.Instant.now();
+        if(StringUtil.isEmpty(serviceName)) {
+            serviceName = IP;
+        }
+        return String.format(REMOTE_TRACE_ID_PATTERN, remoteAddr, serviceName, PID, SystemUtil.now(), Thread.currentThread().getId());
     }
 }
