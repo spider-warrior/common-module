959f9c19754008d1682eefff80a635fb54fd32e1
yangjian <362961910@qq.com>
2021-05-25 11:32:37 +0800

    完善 BusinessEventMessage

--- trace-common-module/logback-trace/src/main/java/cn/t/common/trace/logback/BusinessEventLogLayout.java
+++ trace-common-module/logback-trace/src/main/java/cn/t/common/trace/logback/BusinessEventLogLayout.java
@@ -8,6 +8,7 @@ import cn.t.common.trace.logback.message.BusinessEventMessage;
 import cn.t.util.common.ArrayUtil;
 import cn.t.util.common.DateUtil;
 import cn.t.util.common.JsonUtil;
+import cn.t.util.common.SystemUtil;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import java.util.Date;
@@ -22,9 +23,12 @@ import java.util.Map;
  **/
 public class BusinessEventLogLayout extends LayoutBase<ILoggingEvent> {
 
+    private static final String CURRENT_IP = SystemUtil.getLocalIpV4(true);
+
     private static final String time = "time";
     private static final String traceId = "traceId";
     private static final String clientId = "clientId";
+    private static final String hostname = "hostname";
     private static final String appName = "appName";
     private static final String userId = "userId";
     private static final String eventType = "eventType";
@@ -36,6 +40,7 @@ public class BusinessEventLogLayout extends LayoutBase<ILoggingEvent> {
         map.put(time, DateUtil.convertToZonedDateTimeString((new Date(event.getTimeStamp()))));
         map.put(traceId, event.getMDCPropertyMap().get(TraceConstants.TRACE_ID_NAME));
         map.put(clientId, event.getMDCPropertyMap().get(TraceConstants.CLIENT_ID_NAME));
+        map.put(hostname, CURRENT_IP);
         map.put(appName, event.getLoggerContextVO().getPropertyMap().get(TraceConstants.TRACE_APP_NAME));
         Object[] paramArray = event.getArgumentArray();
         if(!ArrayUtil.isEmpty(paramArray) && paramArray[0] instanceof BusinessEventMessage) {
