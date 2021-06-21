3c0f81e3474cc84365532114af1390b3851c4c8f
yangjian <362961910@qq.com>
2021-06-21 15:05:56 +0800

    完善 ErrorInfoEnum

--- common-service/src/main/java/cn/t/common/service/ErrorInfoEnum.java
+++ common-service/src/main/java/cn/t/common/service/ErrorInfoEnum.java
@@ -6,6 +6,7 @@ public enum ErrorInfoEnum {
     SOURCE_NOT_FOUND(new ErrorInfo("404", "资源不存在")),
     METHOD_NOT_SUPPORT(new ErrorInfo("405", "请求方法不支持")),
     MEDIA_TYPE_NOT_SUPPORT(new ErrorInfo("415", "请求媒体类型不支持")),
+    TOO_MANY_REQUESTS(new ErrorInfo("429", "操作过于频繁")),
     SERVER_INTERNAL_ERROR(new ErrorInfo("500", "服务器内部异常"));
 
     public final ErrorInfo errorInfo;
