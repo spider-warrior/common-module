b470c6903c9894ace72a924f30847f507ff40f16
36296 <362961910@qq.com>
2023-06-08 16:34:38 +0800

    1.0.10.RELEASE

--- common-trace/common-logback-trace/src/main/java/cn/t/common/trace/logback/MimLogContext.java
+++ common-trace/common-logback-trace/src/main/java/cn/t/common/trace/logback/MimLogContext.java
@@ -1,19 +1,17 @@
 package cn.t.common.trace.logback;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MimLogContext {
-    private static final ThreadLocal<Map<String, Object>> threadLogPropertiesThreadLocal = new ThreadLocal<>();
-    public static void setLogProperties(Map<String, Object> properties) {
-        if (properties == null) {
-            return;
-        }
-        threadLogPropertiesThreadLocal.set(properties);
-    }
-    public static Map<String, Object> getLogProperties() {
+    private static final ThreadLocal<Map<String, Object>> threadLogPropertiesThreadLocal = ThreadLocal.withInitial(HashMap::new);
+    public static Map<String, Object> threadLogPropertyMap() {
         return threadLogPropertiesThreadLocal.get();
     }
-    public static void removeLogProperties() {
-        threadLogPropertiesThreadLocal.remove();
+    public static void addLogProperty(String key, Object value) {
+        threadLogPropertiesThreadLocal.get().put(key, value);
+    }
+    public static void clearLogProperties() {
+        threadLogPropertiesThreadLocal.get().clear();
     }
 }
